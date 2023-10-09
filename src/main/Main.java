b7f1e217b09c4c21426f8466a9b14a8fa9a66df8
Dennis Kraus <107929075+KrausDennis@users.noreply.github.com>
2023-10-09 15:06:34 +0200

    todo

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,24 @@ package main;
 
 public class Main {
 
+	/**
+	 * TODO: Create an abstract sealed class "Shape" allowing Circle and Rectangle
+	 * to inherit from 
+	 * 
+	 * TODO: Create a final class Circle inheriting from Shape with the attribute
+	 * 'radius' 
+	 * 
+	 * TODO: Create a final class Rectangle inheriting from Shape with the
+	 * attributes 'a' and 'b'
+	 * 
+	 * TODO: Create a method accepting a 'Shape', which returns the surface of the
+	 * given Shape using a switch expression with a type pattern 
+	 * 
+	 * TODO: Write a method listing all classes inheriting from the 'Shape' class
+	 * 
+	 */
 	public static void main(String[] args) {
-		TaskOne_CreateARecordAndUseIt.task();
+
 	}
 
 }
