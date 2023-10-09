c28b23f6847013e677d707730911aa676ba20590
Dennis Kraus <107929075+KrausDennis@users.noreply.github.com>
2023-10-09 15:17:52 +0200

    exercises

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,22 +3,9 @@ package main;
 public class Main {
 
 	/**
-	 * TODO: Create an sealed interface "Shape" allowing Circle, Rectangle and Triangle
-	 * to inherit from 
+	 * TODO: Create a text block containing a square of '*' with a side length of 4
 	 * 
-	 * TODO: Create a final class Circle inheriting from Shape with the attribute
-	 * 'radius' 
-	 * 
-	 * TODO: Create a final class Rectangle inheriting from Shape with the
-	 * attributes 'a' and 'b'
-	 * 
-	 * TODO: Create a record Triangle inheriting from Shape with the
-	 * attributes 'b' and 'h'
-	 * 
-	 * TODO: Create a method accepting a 'Shape', which returns the surface of the
-	 * given Shape using a switch expression with a type pattern 
-	 * 
-	 * TODO: Write a method listing all classes inheriting from the 'Shape' class
+	 * TODO: Create a text block with a placeholder, which gets inserted via the format function
 	 * 
 	 */
 	public static void main(String[] args) {
