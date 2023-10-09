009c87a018f7f565d5a36bbf0303f00b0eb4744b
Dennis Kraus <107929075+KrausDennis@users.noreply.github.com>
2023-10-09 14:50:02 +0200

    intial task

---
+++ src/main/TaskOne_CreateARecordAndUseIt.java
@@ -0,0 +1,20 @@
+package main;
+
+public class TaskOne_CreateARecordAndUseIt {
+
+	/**
+	 * 1. Create a record named "Rectangle" with a and b side lengths.
+	 * 
+	 * 2. Create a second constructor with a single value, which will be used for a
+	 * and b (=Square)
+	 * 
+	 * 3. Check for the canonical and the new constructor, that the entered values
+	 * have to be greater then zero. Otherwise throw an exception <br>
+	 * 
+	 * 4. Create a static method which returns a new record instance with same side lengths (calling the "square" constructor)
+	 * 
+	 * 5. Create a public method calculating the surface of the record.
+	 * 
+	 */
+
+}
--- src/main/java9/Underscore.java
+++
@@ -1,9 +0,0 @@
-package main.java9;
-
-public class Underscore {
-
-	public void f() {
-		//int _ = 10;
-	}
-	
-}
