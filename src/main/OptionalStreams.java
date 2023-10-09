bd70b4ddc86fef8b9b3ffc24713da0f74e3d4efe
Dennis Kraus <107929075+KrausDennis@users.noreply.github.com>
2023-10-09 13:44:56 +0200

    added hints

--- src/main/OptionalStreams.java
+++ src/main/OptionalStreams.java
@@ -9,6 +9,9 @@ public class OptionalStreams {
 	 * TODO: Filter out the empty optionals and convert the others by using the
 	 * Stream-API
 	 * 
+	 * Hint: Use flatMap
+	 * Hint: Use Optional::stream
+	 * 
 	 * @return
 	 */
 	public static List<String> filterAndConvert(List<Optional<String>> optionals) {
